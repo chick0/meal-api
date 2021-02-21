4d915079648234e7afb026caa48efaac6218bd8e
chick_0 <chick_0@kakao.com>
2021-02-21 23:50:40 +0900

    스크립트 정리

--- app/views/index.py
+++ app/views/index.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from random import choice
 
 from flask import Blueprint, session
 from flask import render_template
@@ -26,6 +27,10 @@ def index():
     return render_template(
         "index/index.html",
         title="급식",
+        placeholder=choice(["ㅇㅇ초",       "ㅇㅇ중",     "ㅇㅇ고",
+                            "ㅇㅇ초등학교", "ㅇㅇ중학교", "ㅇㅇ고등학교",
+                            "ㅁㅁ초",       "ㅁㅁ중",     "ㅁㅁ고",
+                            "ㅁㅁ초등학교", "ㅁㅁ중학교", "ㅁㅁ고등학교"]),
 
         alert=alert
     )
