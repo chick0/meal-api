dd2843486c5ee67a52e2d98492323f28002cde41
chick_0 <chick_0@ch1ck.xyz>
2021-03-11 02:28:52 +0900

    즐겨찾기 관리 스크립트 업데이트

--- app/static/js/star.js
+++ app/static/js/star.js
@@ -8,7 +8,7 @@ function get_star(){
         localStorage.setItem("star", JSON.stringify(star));
     }
 
-    console.log("[chick_0] 즐겨찾기 불러옴 : "+Object.keys(star));
+    console.log(`[chick_0] 즐겨찾기 불러옴 : ${Object.keys(star)}`);
     return star;
 }
 
@@ -18,7 +18,7 @@ function add_star(school, url){
     star[school] = url;
     localStorage.setItem("star", JSON.stringify(star));
 
-    console.log("[chick_0] 즐겨찾기 추가 함 : "+school);
+    console.log(`[chick_0] 즐겨찾기 추가 함 : ${school}`);
 }
 
 function del_star(school){
@@ -27,18 +27,32 @@ function del_star(school){
     delete star[school];
     localStorage.setItem("star", JSON.stringify(star));
 
-    console.log("[chick_0] 즐겨찾기 삭제 함 : "+school);
+    console.log(`[chick_0] 즐겨찾기 삭제 함 : ${school}`);
 }
 
 function render_star(){
     const star = get_star();
     const keys = Object.keys(star);
 
-    let html = "";
-    for(var key in keys){
-        html += `<li><a class="high l select" href="${star[keys[key]]}">${keys[key]}</a></li>`;
-        console.log("[chick_0] 목록에 추가함 : "+keys[key]);
-    }
+    var render_target = document.getElementById("render_target");
+    render_target.innerHTML = "";
 
-    document.querySelector("#render_target").innerHTML = html;
+    for(var key in keys){
+        var li = document.createElement("li");
+        var a = document.createElement("a");
+
+        if(star[keys[key]].startsWith("/")){
+            a.setAttribute("class", "high l");
+            a.setAttribute("href", star[keys[key]]);
+            a.appendChild(document.createTextNode(keys[key]));
+
+            li.appendChild(a);
+            render_target.appendChild(li);
+
+            console.log(`[chick_0] 목록에 추가함 : ${keys[key]}`);
+        }
+        else{
+            console.log(`[chick_0] 이상한 즐겨찾기 정보를 목록에 추가하지 않음 : ${keys[key]}`)
+        }
+    }
 }
