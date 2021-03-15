8cb5985370c16d738d67bfbb555f556e0c45ada1
chick_0 <chick_0@ch1ck.xyz>
2021-03-15 15:38:29 +0900

    서버 시작 스크립트 수정

--- production.py
+++ production.py
@@ -1,37 +1,55 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from os import path, mkdir
+from os import urandom, stat
+from os import path, mkdir, rename
 from datetime import datetime
-from logging import getLogger, FileHandler
+from logging import getLogger
+from logging import FileHandler
 
 from waitress import serve
 from paste.translogger import TransLogger
 
-import app
+from app import create_app
 from conf import conf
 
 
-LOG_PATH = path.join("log")
-
 if __name__ == "__main__":
-    if not path.isdir(path.join(LOG_PATH)):
-        mkdir(LOG_PATH)
+    # 기존 서버 로그가 있다면
+    if path.exists("wsgi.log"):
+        # 기존 로그 보관 풀더가 있는지 검사, 없으면 만들기
+        if not path.exists("log archive"):
+            mkdir("log archive")
 
-    logger = getLogger(
-        name="wsgi"
-    )
+        # 기존 로그의 사이즈가 0이 아니라면 보관 풀더로 이동
+        if stat("wsgi.log").st_size != 0:
+            # 기존 로그의 생성 날짜 가져오기
+            last = datetime.fromtimestamp(path.getctime("wsgi.log"))
 
-    logger.addHandler(
-        hdlr=FileHandler(
-            filename=path.join(LOG_PATH, f"{datetime.today().strftime('%Y-%m-%d %Hh %Mm %Ss')}.log")
-        )
-    )
+            def archive():
+                # 중복방지를 위한 랜덤 코드 만들기
+                code = urandom(4).hex()
 
-    app = app.create_app()
+                try:
+                    # 만든 랜덤코드와 기존 로그의 생성 날짜를 이용해 보관 풀더로 이동
+                    rename(
+                        "wsgi.log",
+                        path.join("log archive", last.strftime(f"%Y-%m-%d %Hh %Mm %Ss {code}.log"))
+                    )
+                except FileExistsError:
+                    # 랜덤코드가 겹치면 다시 시도하기
+                    archive()
+
+            # 기존 로그파일 보관하는 함수 실행하기
+            archive()
+
+    # 로거 핸들러 가져오기, 파일 핸들러 등록하기
+    logger = getLogger("wsgi")
+    logger.addHandler(FileHandler("wsgi.log"))
+
+    # `conf/server.ini`에 설정된 포트로 웹 서버 시작
     serve(
         app=TransLogger(
-            application=app,
-            setup_console_handler=True
+            application=create_app()
         ),
-        port=conf['server']['port']
+        port=conf['server']['port'],
     )
