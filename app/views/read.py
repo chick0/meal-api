390f2ef0a8cc0a2c2b52a59ec9d9b2b8dbf046c1
chick_0 <chick_0@ch1ck.xyz>
2021-03-20 03:59:42 +0900

    api 삭제

--- app/views/read.py
+++ app/views/read.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
+from json import dumps
 from random import choice
 
 from flask import Blueprint
 from flask import request, session
-from flask import render_template, redirect, url_for
+from flask import render_template, Response
+from flask import redirect, url_for
 
 import read
 
@@ -13,6 +15,31 @@ bp = Blueprint(
     import_name=__name__,
     url_prefix=f"/{__name__.split('.')[-1]}"
 )
+
+
+@bp.route("/get")
+def get():
+    # 등록된 시 중에서 한 가지 추출
+    ctx = getattr(read, choice(read.__all__))
+
+    # 작품에서 한 구절 랜점 추첨
+    preview = choice([text for text in ctx.CONTENT if len(text) != 0]).replace("&nbsp;", "")
+
+    # 급식 세션 아이디 가져오기
+    idx = request.args.get("idx", "none")
+
+    # 해당 세션 아이디가 규칙을 따르는지 검사 ( s로 시작하는가 , 5자 인가 )
+    if not idx.startswith("s") or len(idx) != 5:
+        idx = None
+
+    return Response(
+        status=200,
+        mimetype="application/json",
+        response=dumps({
+            "url": url_for("read.show", author=ctx.AUTHOR, title=ctx.TITLE, idx=idx),
+            "preview": preview
+        })
+    )
 
 
 @bp.route("")
