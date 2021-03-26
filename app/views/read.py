4c1112e16b1d433e21af45bdc806aada4b3de72e
chick_0 <chick_0@ch1ck.xyz>
2021-03-27 03:32:15 +0900

    시 작품 목록 & 랜덤 페이지 삭제

--- app/views/read.py
+++ app/views/read.py
@@ -42,38 +42,6 @@ def get():
     )
 
 
-@bp.route("")
-def index():
-    # 시 목록 불러오고, 제목순으로 정렬
-    context = sorted([getattr(read, _id) for _id in read.__all__], key=lambda r: r.TITLE)
-
-    # 급식 조회중인 학교 세션 아이디로 정보 불러오기
-    idx = request.args.get("idx", "none")
-
-    # 해당 세션 아이디가 규칙을 따르는지 검사 ( s로 시작하는가 , 5자 인가 )
-    if idx.startswith("s") and len(idx) == 5 and idx in session.keys():
-        button = session[idx]['name']
-        target = url_for("meal.show",
-                         edu_code=session[idx]['edu'],        # 교육청 코드
-                         school_code=session[idx]['school'],  # 학교 코드
-                         date=session[idx]['date'])           # 날짜 정보
-    else:
-        button = "학교 검색하러 가기"
-        target = url_for("index.index")
-        idx = None
-
-    return render_template(
-        "read/index.html",
-        title="시",
-
-        context=context,   # 작품들
-
-        idx=idx,           # 세션 ID
-        button=button,     # 뒤로가기 버튼 텍스트
-        target=target      # 뒤로가기 버튼 목적지
-    )
-
-
 @bp.route("/<string:author>/<string:title>")
 def show(author: str, title: str):
     try:
@@ -89,7 +57,7 @@ def show(author: str, title: str):
 
     # 해당 세션 아이디가 규칙을 따르는지 검사 ( s로 시작하는가 , 5자 인가 )
     if idx.startswith("s") and len(idx) == 5 and idx in session.keys():
-        button = session[idx]['name']
+        button = f"'{session[idx]['name']}'로 돌아가기"
         target = url_for("meal.show",
                          edu_code=session[idx]['edu'],        # 교육청 코드
                          school_code=session[idx]['school'],  # 학교 코드
@@ -108,16 +76,4 @@ def show(author: str, title: str):
         idx=idx,           # 세션 ID
         button=button,     # 뒤로가기 버튼 텍스트
         target=target      # 뒤로가기 버튼 목적지
-    )
-
-
-@bp.route("/random")
-def random():
-    # 등록된 시 중에서 한 가지 추출
-    ctx = getattr(read, choice(read.__all__))
-
-    return render_template(
-        "read/random.html",
-        title=ctx.TITLE,
-        ctx=ctx
     )
