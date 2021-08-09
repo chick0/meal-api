8ede16f78abd9a4a4af302a85fcf1d635d808e13
chick_0 <chick_0@ch1ck.xyz>
2021-08-09 14:12:37 +0900

    업데이트

--- app/error.py
+++ app/error.py
@@ -6,7 +6,6 @@ def bad_request(error):
     return render_template(
         "error/error.html",
         title="잘못된 요청",
-
         message="잘못된 요청입니다"
     ), getattr(error, "code")
 
@@ -15,7 +14,6 @@ def forbidden(error):
     return render_template(
         "error/error.html",
         title="잘못된 요청",
-
         message="접근할 수 없습니다"
     ), getattr(error, "code")
 
@@ -24,7 +22,6 @@ def page_not_found(error):
     return render_template(
         "error/error.html",
         title="페이지를 찾을 수 없음",
-
         message="해당 페이지를 찾을 수 없습니다"
     ), getattr(error, "code")
 
@@ -33,7 +30,6 @@ def method_not_allowed(error):
     return render_template(
         "error/error.html",
         title="잘못된 요청",
-
         message="해당 요청 방식은 사용할 수 없습니다"
     ), getattr(error, "code")
 
@@ -42,6 +38,5 @@ def internal_server_error(error):
     return render_template(
         "error/error.html",
         title="스크립트 오류 발생",
-
         message="내부 스크립트 오류가 발생하였습니다"
     ), getattr(error, "code")
