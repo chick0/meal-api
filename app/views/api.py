8b3324cd7050cde9344df3ed5519e1e3146b9952
chick_0 <chick_0@ch1ck.xyz>
2022-01-17 23:31:59 +0900

    30일보다 먼 미래의 요청 거부

--- app/views/api.py
+++ app/views/api.py
@@ -56,13 +56,39 @@ def school():
 
 @bp.get("/meal")
 def meal():
+    # 오늘 날짜 불러오기
+    #  -> 시간은 버리기
+    date_format = "%Y" + "%m" + "%d"
+    today = datetime.strptime(datetime.today().strftime(date_format), date_format)
+
+    # 교육청 코드 & 학교 코드 불러오기
     edu_code = request.args.get("edu", None)
     school_code = request.args.get("school", None)
-    date = request.args.get("date", datetime.now().strftime("%Y%m%d"))
 
+    # 조회 요청에 교육청 코드 또는 학교 코드가 없다면
     if edu_code is None or school_code is None:
         return error(code="meal.query_none")
 
+    # 조회 날짜 날짜 불러오기
+    date = request.args.get("date", None)
+
+    # 조회 날짜를 설정하지 않았다면 오늘 날짜로 설정하기
+    if date is None:
+        date = today.strftime(date_format)
+
+    # 날짜 형식이 올바르지 않다면
+    if len(date) != 8:
+        return error(code="meal.not_yyyymmdd")
+
+    # 30일보다 먼 미래라면
+    try:
+        if (datetime.strptime(date, date_format) - today).days >= 30:
+            return error(code="meal.30")
+    except ValueError:
+        # 날짜 형식이 올바르지 않아서 파싱에 실패한 상황
+        return error(code="meal.not_yyyymmdd")
+
+    # 데이터 불러오기
     result = get_meal_data_by_codes(
         edu=edu_code,
         school=school_code,
