189cf614cc57536301909a99098dfafc98267e12
chick_0 <chick_0@ch1ck.xyz>
2022-11-04 16:33:26 +0900

    fix: Meal data parsing issue

--- app/meal.py
+++ app/meal.py
@@ -1,3 +1,4 @@
+from re import compile
 from json import dumps
 from json import loads
 from urllib.error import HTTPError
@@ -36,20 +37,30 @@ def reformat(json: list):
 
     new_json = []
 
+    re = compile(r"([0-9.]){2,}")
+
     for item in json:
         menu_list = []
         for menu in item['DDISH_NM'].split("<br/>"):
-            display, codes = menu.split("  ")
+            search = re.search(menu)
 
-            codes = [int(x)
-                     # 괄호 제거 & . 을 기준으로 자르기
-                     for x in codes.replace("(", "").replace(")", "").split(".") if len(x) != 0]
+            if search is None:
+                display = menu
+                allergy = []
+                codes = []
+            else:
+                codes = menu[search.start():search.end()]
+                display = menu.replace(codes, "")
 
-            allergy = []
-            for code in codes:
-                # 알러지 식품명 표에서 가져오기
-                # 표에 등록된 식품이 아니라면 그냥 숫자 코드 등록하기
-                allergy.append(table.get(code, str(code)))
+                codes = [int(x) for x in codes.split(".") if len(x) != 0]
+
+                allergy = []
+                for code in codes:
+                    # 알러지 식품명 표에서 가져오기
+                    # 표에 등록된 식품이 아니라면 그냥 숫자 코드 등록하기
+                    allergy.append(table.get(code, str(code)))
+
+            display = display.replace("()", "").strip()
 
             menu_list.append({
                 "name": display,
@@ -72,16 +83,20 @@ def reformat(json: list):
 
 
 def get_meal_data_by_codes(edu: str, school: str, date: str):
+    def addr(sep: str = " ") -> str:
+        return sep.join([edu, school, date])
+
     def fetch_from_redis() -> list or None:
         if app.redis is None:
             return None
 
         try:
-            from_redis = app.redis.get(name=f"{edu}#{school}#{date}")
+            from_redis = app.redis.get(name=addr("#"))
 
             if from_redis is None:
                 return None
 
+            logger.info(f"Get meal date from redis / {addr()}")
             return loads(from_redis)
         except (ConnectionError, TypeError, Exception):
             logger.exception("Exception in getting data from redis")
@@ -90,10 +105,12 @@ def get_meal_data_by_codes(edu: str, school: str, date: str):
     def add_cache(json: list):
         try:
             app.redis.set(
-                name=f"{edu}#{school}#{date}",
+                name=addr("#"),
                 value=dumps(json),
                 ex=604800
             )
+
+            logger.info(f"Add meal data to redis / {addr()}")
         except ConnectionError:
             pass
 
@@ -111,6 +128,7 @@ def get_meal_data_by_codes(edu: str, school: str, date: str):
                 logger.exception("Exception in meal data api request")
                 return False
 
+        logger.info(f"Get meal date from api / {addr()}")
         result = fetch_from_api()
 
         if isinstance(result, dict):
