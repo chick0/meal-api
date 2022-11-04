8ab10bacc75bf729ca683eba9e86137d335d946e
chick_0 <chick_0@ch1ck.xyz>
2022-11-04 14:31:23 +0900

    chore: Change api error message

--- app/error.py
+++ app/error.py
@@ -1,14 +1,14 @@
 ERRORS = {
     "school": {
-        "query_filtered": "해당 검색어는 사용이 불가능 합니다",
-        "result_none": "검색 결과가 없습니다",
-        "api_timeout_or_dead": "학교 목록을 불러오는 데 실패했습니다",
+        "query_filtered": "해당 검색어는 사용할 수 없습니다.",
+        "result_none": "검색 결과가 없습니다.",
+        "api_timeout_or_dead": "학교 목록을 불러오는 데 실패했습니다.",
     },
     "meal": {
-        "query_none": "교육청 코드와 학교 코드를 전달받지 못했습니다",
-        "result_none": "급식 정보가 없습니다",
-        "api_timeout_or_dead": "급식 정보를 불러오는 데 실패했습니다",
-        "not_yyyymmdd": "요청한 날짜가 올바르지 않습니다. YYYYMMDD 형식을 지켜주세요."
+        "result_none": "급식 정보가 없습니다.",
+        "api_timeout_or_dead": "급식 정보를 불러오는 데 실패했습니다.",
+        "query_none": "요청오류) 교육청 코드와 학교 코드를 전달받지 못했습니다.",
+        "not_yyyymmdd": "요청오류) 요청 날짜의 YYYYMMDD 형식을 지켜주세요."
     },
 }
 
