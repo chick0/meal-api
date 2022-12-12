36f9e01676a12da70b8d02fecb39db7faaf3f96e
chick_0 <chick_0@ch1ck.xyz>
2022-12-13 06:51:53 +0900

    chore: Use logger in create_app function

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from os import environ
 from sys import exit
-from sys import stderr
+from logging import getLogger
 
 from flask import Flask
 from redis import Redis
 from dotenv import load_dotenv
+
+logger = getLogger()
 
 if "REDIS_URL" not in environ:
     load_dotenv()
@@ -13,19 +15,20 @@ if "REDIS_URL" not in environ:
 def create_app():
     app = Flask(__name__, static_folder=None)
 
-    REDIS_URL = environ.get("REDIS_URL", default=None)
+    try:
+        REDIS_URL = environ['REDIS_URL'].strip()
 
-    if REDIS_URL is None:
-        app.redis = None  # type: ignore
-    else:
         app.redis = Redis.from_url(  # type: ignore
             url=REDIS_URL
         )
+    except KeyError:
+        logger.warning("환경변수에 REDIS_URL이 지정되지 않았습니다!")
+        app.redis = None  # type: ignore
 
-    API_KEY = environ.get("API_KEY", default="#")
-
-    if API_KEY == "#":
-        stderr.write("API 서버를 시작할 수 없습니다. '나이스 교육정보 개방 포털'의 API 키가 필요합니다.")
+    try:
+        API_KEY = environ['API_KEY'].strip()
+    except KeyError:
+        logger.error("API 서버를 시작할 수 없습니다. '나이스 교육정보 개방 포털'의 API 키가 필요합니다.")
         exit(-1)
 
     app.API_KEY = API_KEY  # type: ignore
